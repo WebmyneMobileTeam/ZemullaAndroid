ad58bed8b261648f2094d0ab87c6a02ef3bb3327
dhruvil <dd.droid@yahoo.com>
2016-07-29 15:45:49 +0530

    uyt

--- app/src/main/java/com/zemulla/android/app/topup/TopupActivity.java
+++ app/src/main/java/com/zemulla/android/app/topup/TopupActivity.java
@@ -3,6 +3,7 @@ package com.zemulla.android.app.topup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.zemulla.android.app.R;
@@ -15,25 +16,34 @@ public class TopupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topup);
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Dhruvil Patel");
         getSupportActionBar().setSubtitle("Effective Balance : ZMW 1222.5");
+    }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()){
             case android.R.id.home:
                 finish();
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 }

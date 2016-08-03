afc0e2ac0e415ed0379d9f19390528caad6a1d82
sagar.091 <sagar.ucet091@gmail.com>
2016-08-03 18:30:16 +0530

    jrg

--- app/src/main/java/com/zemulla/android/app/topup/cyber/CyberSourceActivity.java
+++ app/src/main/java/com/zemulla/android/app/topup/cyber/CyberSourceActivity.java
@@ -5,28 +5,49 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.zemulla.android.app.R;
 import com.zemulla.android.app.helper.FlipAnimation;
+import com.zemulla.android.app.helper.Functions;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import mbanje.kurt.fabbutton.FabButton;
 
 public class CyberSourceActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private FabButton fabButton;
-    private LinearLayout lineatInitialViewTopup;
-    private LinearLayout linearTrnsViewTopup;
-    private FabButton btnProcessResetTransaction;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.txtTopupWayName)
+    TextView txtTopupWayName;
+    @BindView(R.id.btnProcessInitialTransaction)
+    FabButton btnProcessInitialTransaction;
+    @BindView(R.id.lineatInitialViewTopup)
+    LinearLayout lineatInitialViewTopup;
+    @BindView(R.id.btnProcessResetTransaction)
+    FabButton btnProcessResetTransaction;
+    @BindView(R.id.btnProcessConfirmTransaction)
+    FabButton btnProcessConfirmTransaction;
+    @BindView(R.id.linearTrnsViewTopup)
+    LinearLayout linearTrnsViewTopup;
+    @BindView(R.id.frameRootTopup)
+    FrameLayout frameRootTopup;
+    @BindView(R.id.activity_topup_initial_transaction)
+    LinearLayout activityTopupInitialTransaction;
+    @BindView(R.id.edtAmount)
+    EditText edtAmount;
+
     private FlipAnimation animation;
-    FrameLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cyber_source);
+        ButterKnife.bind(this);
 
         init();
     }
@@ -34,50 +55,53 @@ public class CyberSourceActivity extends AppCompatActivity {
     private void init() {
         initToolbar();
 
-        lineatInitialViewTopup = (LinearLayout) findViewById(R.id.lineatInitialViewTopup);
-        linearTrnsViewTopup = (LinearLayout) findViewById(R.id.linearTrnsViewTopup);
+        actionListener();
+    }
 
-
-        fabButton = (FabButton) findViewById(R.id.btnProcessInitialTransaction);
-        rootLayout = (FrameLayout) findViewById(R.id.frameRootTopup);
-
-        fabButton.setOnClickListener(new View.OnClickListener() {
+    private void actionListener() {
+        btnProcessInitialTransaction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fabButton.showProgress(true);
 
-                new CountDownTimer(3000, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-
-                    }
-
-                    @Override
-                    public void onFinish() {
-
-                        fabButton.hideProgressOnComplete(true);
-                        fabButton.onProgressCompleted();
-                        animation = new FlipAnimation(lineatInitialViewTopup, linearTrnsViewTopup);
-                        rootLayout.startAnimation(animation);
-
-                    }
-                }.start();
-
+                if (Functions.isEmpty(edtAmount)) {
+                    Functions.showError(CyberSourceActivity.this, "Please Enter Amount", false);
+                } else {
+                    calculateAmount();
+                }
             }
         });
 
-        btnProcessResetTransaction = (FabButton) findViewById(R.id.btnProcessResetTransaction);
         btnProcessResetTransaction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 animation.reverse();
-                rootLayout.startAnimation(animation);
+                frameRootTopup.startAnimation(animation);
             }
         });
     }
 
+    private void calculateAmount() {
+        btnProcessInitialTransaction.showProgress(true);
+
+        new CountDownTimer(2000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                btnProcessInitialTransaction.hideProgressOnComplete(true);
+                btnProcessInitialTransaction.onProgressCompleted();
+                animation = new FlipAnimation(lineatInitialViewTopup, linearTrnsViewTopup);
+                frameRootTopup.startAnimation(animation);
+
+            }
+        }.start();
+    }
+
     private void initToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             toolbar.setTitle("Dhruvil Patel");
             toolbar.setSubtitle("Effective Balance : ZMW 1222.5");
